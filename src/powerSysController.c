84a7c0ed6cd5d66fa67bd54a09f000a7a75c1f8f
itchy8me <itchy8me@gmail.com>
2014-05-29 22:10:32 +0200

    CHANGELOG: 1)Added strobe patterns

--- src/powerSysController.c
+++ src/powerSysController.c
@@ -1,22 +1,30 @@
 /*
- * powerSysController.c
- */
+* powerSysController.c
+*/
 #include "../includes/system_init.h"
+static volatile int strobe_sequence_A[] = {LED_R};
+static volatile int * strobe_sequence = strobe_sequence_A;
+static volatile int strobe_active = 0;
+
+void strobe_pattern(int pattern_num){
+	switch (pattern_num){
+		case 0:
+			if(strobe_active == (sizeof(strobe_sequence))){strobe_active = 0;}
+			STROBE(strobe_sequence[strobe_active]);
+			strobe_active++;
+			break;
+	}
+}
 
 int main(void) {
 	CLOCK_CFG;
 	STROBE_ENABLE;
-	//ENABLE_STEPPINGMOTOR();
-	//LED1_OUT |= 0x0A; //0x0E;
-	//LED1_DEN |= 0x0A; //0x0E;
-	//LED1OUTT = 0x00000002;
-	//LED1UNLOCK = 0x4C4F434B;
-	//LED1COMMIT = 0x00000002;
 
 	unsigned long i = 0;
 	unsigned long ulLoop = 0;
 	while(1){
-		STROBE(LED_YELLOW);
+		strobe_pattern(0);
+		//STROBE(LED_CYAN);
 		for(ulLoop = 0; ulLoop < 500000; ulLoop++);
 	}
 		//return(0);
