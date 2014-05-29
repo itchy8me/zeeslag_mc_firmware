d1117c4a02190004d629dba45e63ad539d56ac03
Wernher Korff <itchy8me@gmail.com>
2014-05-29 20:51:55 +0200

    CHANGELOG: 1) Added defines for status strobe

--- includes/system_init.h
+++ includes/system_init.h
@@ -1,36 +1,26 @@
- 
-#define REGGET(x) *((volatile unsigned int *)(x))
+#include "inc/lm4f120h5qr.h"
+//#include "inc/hw_gpio.h"
 
-#define SCRB REGGET(0x400FE000)	// System configure register
-#define CLKGATING REGGET(0x400FE608) // Clock ouput pin gating
+#define CLOCK_CFG (SYSCTL_RCC_R = 0x02400540)	// Set clock divider
 
-/* GPIO configuration and data transfer */
-#define GPIOAFSEL_PORTC REGGET(0x40006420)
+#define BITF0 0x00000001
+#define BITF1 0x00000002
+#define BITF2 0x00000004
+#define BITF3 0x00000008
+
+#define LED_R BITF1
+#define LED_B BITF2
+#define LED_G BITF3
+#define LED_YELLOW (LED_R | LED_B & ~LED_R)
+#define LED_WHITE (LED_R | LED_B | LED_G)
+
+#define STROBE_ENABLE ((SYSCTL_RCGCGPIO_R = SYSCTL_RCGCGPIO_R5) & (GPIO_PORTF_DIR_R = LED_WHITE) & (GPIO_PORTF_DR4R_R = LED_WHITE) & (GPIO_PORTF_DEN_R = LED_WHITE) & (GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY) & (GPIO_PORTF_CR_R = LED_WHITE))	//Enable port F1-F3 for strobe status
+#define STROBE(x) ((GPIO_PORTF_DATA_R ^= x))
 
 
-/* Array of ports */
-#define GPIODIR_PORTC REGGET(0x40006400)	// I/O direction 
-#define GPIODIR_PORTF REGGET(0x40025400)	// I/O direction
 
-/* General purpose timer configuration */
-#define GPTMCFG_T0 REGGET(0x40036000)
-
-/* STEPPINGMOTOR INIT */
-#define ENABLE_STROBE ((CLKGATING |= 0x20) & (GPIODIR_PORTF |= 0x02)) // Just for aesthetic pleasure
-#define ENABLE_STEPPINGMOTOR() ((CLKGATING |= 0x04) & (GPIODIR_PORTC |= 0x10) & (GPIOAFSEL_PORTC |= 0x07) & (GPTMCFG_T0 &= ~(0x01))) // Stepping motor (problem with GPTMCFG)
-#define CLOCK REGGET(0x400FE060)
-//#define LED1_OUT REGGET()
-#define LED1_DEN REGGET(0x4002551C)
-#define LED1_ON REGGET(0x400253FC)
-#define LED1OUTT REGGET(0x40025504)
-
-#define LED1UNLOCK REGGET(0x40025520)
-#define LED1COMMIT REGGET(0x40025524)
-#define LED
-  
-// #define step_motor() (RCGCGPIO |= (PC4 << 1) &\
-// 		      GPIOAFSEL |= () &\
-// 		      GPIOPCTL |= () &\
-// 		      )
-		      
- int system_init();'\
+#define MOTORA 0x00000001
+#define MOTORB 0x00000002
+#define MOTORC 0x00000004
+#define MOTORD 0x00000008
+#define STEPPING_MOTOR_EN(x)
--- src/powerSysController.c
+++ src/powerSysController.c
@@ -4,12 +4,11 @@
 #include "../includes/system_init.h"
 
 int main(void) {
-	CLOCK = 0x02400540;
-
-	ENABLE_STROBE;
-	ENABLE_STEPPINGMOTOR();
+	CLOCK_CFG;
+	STROBE_ENABLE;
+	//ENABLE_STEPPINGMOTOR();
 	//LED1_OUT |= 0x0A; //0x0E;
-	LED1_DEN |= 0x0A; //0x0E;
+	//LED1_DEN |= 0x0A; //0x0E;
 	//LED1OUTT = 0x00000002;
 	//LED1UNLOCK = 0x4C4F434B;
 	//LED1COMMIT = 0x00000002;
@@ -17,7 +16,7 @@ int main(void) {
 	unsigned long i = 0;
 	unsigned long ulLoop = 0;
 	while(1){
-		LED1_ON = i++;
+		STROBE(LED_YELLOW);
 		for(ulLoop = 0; ulLoop < 500000; ulLoop++);
 	}
 		//return(0);
