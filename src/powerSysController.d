21685b5a2ddd769c34d4e7691b15a2cabcc6836a
itchy8me <itchy8me@gmail.com>
2014-05-30 22:08:31 +0200

    CHANGELOG: 1) Added timer configuration for DC_MOTORS

--- src/powerSysController.d
+++ src/powerSysController.d
@@ -1,1 +1,8 @@
-powerSysController.o: powerSysController.c ../includes/system_init.h
+powerSysController.o: powerSysController.c \
+ ../includes/powerSysController.h \
+ /home/itchy8me/stellarisware/inc/lm4f120h5qr.h \
+ ../includes/../includes/system_init.h \
+ ../includes/../includes/strobe_status.h \
+ ../includes/../includes/../includes/system_init.h \
+ ../includes/../includes/dc_motor.h \
+ ../includes/../includes/stepping_motor.h
--- src/system_init.d
+++ src/system_init.d
@@ -1,1 +1,2 @@
-system_init.o: system_init.c ../includes/system_init.h
+system_init.o: system_init.c ../includes/system_init.h \
+ /home/itchy8me/stellarisware/inc/lm4f120h5qr.h
